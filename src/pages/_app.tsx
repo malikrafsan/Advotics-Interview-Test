44388c2b44602f261350e1c28e3b1491ade97e34
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2022-02-22 15:10:55 +0700

    feat: set up home page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-daterangepicker/daterangepicker.css";
+import "../styles/globals.css";
+import "../styles/period-filter.css";
+import "../styles/accordion.css";
+import "../styles/dropdown.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
